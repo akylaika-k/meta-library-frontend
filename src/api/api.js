168254d2114d0abc2326fcc8ffbaae58abec0362
akylaika-k <akylajkalmurzaeva526@gmail.com>
2026-04-19 00:08:41 +0600

    объединниеи с бэк

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://mm-nu-library-backend.onrender.com";
 
 function getAuthHeaders() {
   const token = localStorage.getItem("token");
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,46 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import API from "../api/api"; 
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/catalog");
+  // Объединенная асинхронная функция для связи с бэкендом
+  const handleLogin = async () => {
+    try {
+      const res = await API.post("/auth/login", { username, password });
+      console.log("Ура, ответ от сервера:", res.data);
+      
+      navigate("/catalog"); 
+    } catch (error) {
+      alert("Ошибка входа! Проверь логин и пароль.");
+      console.error(error);
+    }
   };
 
   return (
     <div className="login-page">
       <div className="login-card">
         
-        
         <div className="login-form-side">
           <h2>Welcome!</h2>
-          <input className="login-input" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-          <input className="login-input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <input 
+            className="login-input" 
+            placeholder="Username" 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+          <input 
+            className="login-input" 
+            type="password" 
+            placeholder="Password" 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
           <button className="login-btn" onClick={handleLogin}>SIGN IN</button>
         </div>
 
-      
         <div className="login-brand-side">
           <h1 style={{ fontSize: "32px", marginBottom: "10px" }}>BookWorm</h1>
           <p style={{ letterSpacing: "2px" }}>LIBRARY</p>
